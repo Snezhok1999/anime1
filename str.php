34df84b2587f478f13f9fc071bbcb17e77980163
Olya <56249046+Snezhok1999@users.noreply.github.com>
2019-12-16 10:51:59 +0300

    Create str.php

---
+++ str.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>s9</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="stili.css">
+</head>
+<body>
+	<?php
+	//if(isset($_SESSION['username']))
+	//{
+	//	header('Location: /log.php');
+	//}
+	//	?>
+		
+		<h1 class="hello"><a class='h2'>Добро пожаловать, кун!</a></h1>
+	<div id="ds1">
+	<form method="post" action="normlog.php" style="font-family: 'Comfortaa', cursive;">
+
+<p>Имя пользователя:<input type="text" name="username" id="in1"></p>
+<p style="margin-left: 82px;">Пароль:<input type="password" name="pass" id="in1"></p>
+<p><input type="submit" name="отправить" class='a-q h2'></p>
+</form>
+<a href="regform.php" class="hello h2">Зарегистрируйся, братик</a>
+</div>
+<div id="ds2">@Star_anime</div>
+		<footer>
+			<span>&copy;2019 Snezhkova O.D. По всем вопросам пишите по адресу olya.snezhok@mail.ru</span>
+		</footer>
+</body>
+</html>
